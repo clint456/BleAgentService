ac8a8070856d4c354d65268a71ef47cba0d571e2
clint <clinton_luo@qq.com>
2025-05-26 23:53:31 +0800

    准备修改编码风格

--- internal/bledriver/bledriver.go
+++ internal/bledriver/bledriver.go
@@ -3,6 +3,7 @@ package bledriver
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/edgexfoundry/device-sdk-go/v3/pkg/interfaces"
 	dsModels "github.com/edgexfoundry/device-sdk-go/v3/pkg/models"
@@ -103,7 +104,8 @@ func (s *BleDriver) HandleReadCommands(deviceName string, protocols map[string]m
 				if err != nil {
 					return nil, fmt.Errorf(createCommandValueError, req.DeviceResourceName, err)
 				}
-			case comm.ValueType
+			case common.ValueTypeObject:
+
 			default:
 				return nil, fmt.Errorf("Driver.HandleReadCommands(): Unsupported value type: %v", valueType)
 			}
@@ -186,6 +188,133 @@ func (s *BleDriver) HandleWriteCommands(deviceName string, protocols map[string]
 
 	}
 	return nil
+}
+
+func newResult(resource models.DeviceResource, reading interface{}) (*dsModels.CommandValue, error) {
+	var err error
+	var result = &dsModels.CommandValue{}
+	castError := "fail to parse %v reading, %v"
+
+	valueType := resource.Properties.ValueType
+
+	if !checkValueInRange(valueType, reading) {
+		err = fmt.Errorf("parse reading fail. Reading %v is out of the value type(%v)'s range", reading, valueType)
+		return result, err
+	}
+
+	var val interface{}
+	switch valueType {
+	case common.ValueTypeBool:
+		val, err = cast.ToBoolE(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeString:
+		val, err = cast.ToStringE(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeUint8:
+		val, err = cast.ToUint8E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeUint16:
+		val, err = cast.ToUint16E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeUint32:
+		val, err = cast.ToUint32E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeUint64:
+		val, err = cast.ToUint64E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeInt8:
+		val, err = cast.ToInt8E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeInt16:
+		val, err = cast.ToInt16E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeInt32:
+		val, err = cast.ToInt32E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeInt64:
+		val, err = cast.ToInt64E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeFloat32:
+		val, err = cast.ToFloat32E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeFloat64:
+		val, err = cast.ToFloat64E(reading)
+		if err != nil {
+			return nil, fmt.Errorf(castError, resource.Name, err)
+		}
+	case common.ValueTypeObject:
+		val = reading
+	default:
+		return nil, fmt.Errorf("return result fail, none supported value type: %v", valueType)
+
+	}
+
+	result, err = dsModels.NewCommandValue(resource.Name, valueType, val)
+	if err != nil {
+		return nil, err
+	}
+	result.Origin = time.Now().UnixNano()
+
+	return result, nil
+}
+
+func newCommandValue(valueType string, param *dsModels.CommandValue) (interface{}, error) {
+	var commandValue interface{}
+	var err error
+	switch valueType {
+	case common.ValueTypeBool:
+		commandValue, err = param.BoolValue()
+	case common.ValueTypeString:
+		commandValue, err = param.StringValue()
+	case common.ValueTypeUint8:
+		commandValue, err = param.Uint8Value()
+	case common.ValueTypeUint16:
+		commandValue, err = param.Uint16Value()
+	case common.ValueTypeUint32:
+		commandValue, err = param.Uint32Value()
+	case common.ValueTypeUint64:
+		commandValue, err = param.Uint64Value()
+	case common.ValueTypeInt8:
+		commandValue, err = param.Int8Value()
+	case common.ValueTypeInt16:
+		commandValue, err = param.Int16Value()
+	case common.ValueTypeInt32:
+		commandValue, err = param.Int32Value()
+	case common.ValueTypeInt64:
+		commandValue, err = param.Int64Value()
+	case common.ValueTypeFloat32:
+		commandValue, err = param.Float32Value()
+	case common.ValueTypeFloat64:
+		commandValue, err = param.Float64Value()
+	case common.ValueTypeObject:
+		commandValue, err = param.ObjectValue()
+	default:
+		err = fmt.Errorf("fail to convert param, none supported value type: %v", valueType)
+	}
+
+	return commandValue, err
 }
 
 // Discover triggers protocol specific device discovery, asynchronously writes
